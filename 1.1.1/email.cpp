2396298aecafbe3329d0bd18d98b250e6067192f
장은우 <oxygenic314@gmail.com>
2025-02-16 02:17:31 +0900

    Update email.cpp

--- 1.1.1/email.cpp
+++ 1.1.1/email.cpp
@@ -8,8 +8,8 @@ void sendEmail(const char* subject, const char* messages) {
   // SMTP 서버 정보 설정
   config.server.host_name = "smtp.gmail.com";
   config.server.port = 465;
-  config.login.email = "oxygenic314@gmail.com";
-  config.login.password = "rtksqfisuzrswtpu";
+  config.login.email = "@gmail.com";
+  config.login.password = "PASSWORD";
   config.time.ntp_server = F("pool.ntp.org");
 
   // SMTP 세션 연결
@@ -18,10 +18,10 @@ void sendEmail(const char* subject, const char* messages) {
   // 이메일 메시지 생성
   SMTP_Message message;
 
-  message.sender.name = "NodeMCU v1.1.0";
-  message.sender.email = "oxygenic314@gmail.com";
+  message.sender.name = "NodeMCU v1.1.1";
+  message.sender.email = "sender@gmail.com";
   message.subject = subject;
-  message.addRecipient("장은우", "oxygenic@korea.ac.kr");
+  message.addRecipient("recipient", "recipient@gmail.com");
   message.text.content = messages;
   message.text.charSet = "utf-8";
   message.text.transfer_encoding = Content_Transfer_Encoding::enc_7bit;
