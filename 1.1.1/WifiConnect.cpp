2ef39e4b99859da03b968d80159f5a5ce88e3375
장은우 <oxygenic314@gmail.com>
2025-02-17 01:41:01 +0900

    Update WifiConnect.cpp

--- 1.1.1/WifiConnect.cpp
+++ 1.1.1/WifiConnect.cpp
@@ -1,8 +1,8 @@
 #include "WifiConnect.h"
 
 void WifiConnect::connect() {
-  // WiFi.begin("sorim445_2G", "earlyuniverse?");
-  WiFi.begin("U+Net67B0", "physical3!4");
+  // WiFi.begin("SSID1", "PASSWORD1");
+  WiFi.begin("SSID2", "PASSWORD2");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     DEBUG_PRINT(".");
